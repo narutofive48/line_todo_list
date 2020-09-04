295ca8a01d5d00ca08f6de0880b65d1023092753
小島英夫 <>
2020-09-04 20:40:21 +0900

    プログラムのエラー発生の原因が分からずいろいろな方法を試した結果です

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  # binding.pry
   resources :tasks
   root "tasks#index"
   post "/callback" => "line_bot#callback"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
